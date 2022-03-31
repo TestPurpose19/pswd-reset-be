bb97ed75fd508308ace71642fe2b05849de14b41
SaviSanjay1310 <savithav1310@gmail.com>
2022-04-01 01:37:02 +0530

    Update

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const connection = require("./db");
+const connection = require("./db.js");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const passwordResetRoutes = require("./routes/passwordReset");
+
 
 // database connection
 connection();
@@ -15,13 +16,14 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', function (req, res) {
-    res.send('Working')
+    res.send('Hurray...Server is Working!!!')
   })
 
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/password-reset", passwordResetRoutes);
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
